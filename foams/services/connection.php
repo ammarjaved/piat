03e5e256ea109d3261e91ce987c88fb8e0ef8093
arehman5110 <a.rehman5110@gmail.com>
2023-08-21 14:09:24 +0500

    Update connection.php

--- foams/services/connection.php
+++ foams/services/connection.php
@@ -1,11 +1,11 @@
  <?php
 
 
-    $hostname    = 'localhost'; 
-    $port        =  5432;
+    $hostname = '121.121.232.54'; 
+    $port        =  5433;
     $database    = 'piat_checklist';
     $username    = 'postgres';
-    $password    = '123';
+    $password    = 'Admin123';
  
     $pdo = new PDO("pgsql:host=$hostname;dbname=$database;port=$port", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
