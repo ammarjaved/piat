812994742ab0457f85ff93b6880b63a1a14cc699
progmaster001 <104674157+progmaster001@users.noreply.github.com>
2023-08-23 14:54:06 +0500

    update add login module

--- src/auth/login.php
+++ src/auth/login.php
@@ -24,7 +24,12 @@ session_start();
 <body>
 
 
-<?php include "../partials/navbar.php" ;?>
+<nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/src/index.php" style="font-weight: 600;"> Piat Check List</a>
+ 
+  </div>
+</nav>
 
 <?php
 
--- src/partials/navbar.php
+++ src/partials/navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm">
   <div class="container-fluid">
     <a class="navbar-brand" href="/src/index.php" style="font-weight: 600;"> Piat Check List</a>
-   
+    <a href="../auth/logout.php" class="btn btn-sm btn-secondary">logout</a>
   </div>
 </nav>
